67fb6d9a80b8df0832bb960a0d183a3f366b8e4b
vgishyan <vahagn.gishyan.a@gmail.com>
2022-11-17 15:15:49 +0400

    Add ULForFastTestingWithLog

--- Main.cpp
+++ Main.cpp
@@ -8,25 +8,45 @@
 
 namespace TeamSoftTask
 {
+	class ULForFastTestingWithLog : public UserListenerLog
+	{
+	public:
+		ULForFastTestingWithLog(std::unique_ptr<TaskList> tasklist) : UserListenerLog(std::move(tasklist)) {}
+
+		void StartListening()
+		{
+			UserListenerLog::StartListening();
+			std::cout << "Do you want add some tasks for fast testing ? y/n : ";
+			char symbol = 'a';
+			std::cin >> symbol;
+			while (symbol != 'y' && symbol != 'n')
+			{
+				std::cout << "Please input y or n : ";
+				std::cin >> symbol;
+			}
+			if (symbol == 'y')
+			{
+				Do(ParseInput("add \"some-name-0\" \"some-desc-0\" \"3-5-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"some-name-1\" \"some-desc-1\" \"9993-9-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"some-name-2\" \"some-desc-2\" \"2563-12-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"some-name-3\" \"some-desc-3\" \"2013-10-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"some-name-4\" \"some-desc-4\" \"23-1-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"some-name-5\" \"some-desc-5\" \"0-0-5 19:05\" \"cat1\""));
+
+				Do(ParseInput("add \"any-name-0\" \"any-desc-5\" \"400-0-5 19:05\" \"cat0\""));
+				Do(ParseInput("add \"any-name-1\" \"any-desc-2\" \"0-0-5 19:05\" \"cat1\""));
+				Do(ParseInput("add \"any-name-2\" \"any-desc-2\" \"0-0-5 19:05\" \"cat2\""));
+			}
+		}
+	};
+
 	void Main()
 	{
 		//Temp(); //for fast testing
-		std::unique_ptr<UserListener> listener = std::make_unique<UserListenerLog>(std::move(std::make_unique<TaskList>()));
+		std::unique_ptr<UserListener> listener = std::make_unique<ULForFastTestingWithLog>(std::move(std::make_unique<TaskList>()));
 
 		listener->StartListening();
 		const std::string exit = listener->InputForStopListening(); // input this, for stop working
-		
-		// temp, for fast testing
-		listener->Do(listener->ParseInput("add \"some-name-0\" \"some-desc-0\" \"3-5-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"some-name-1\" \"some-desc-1\" \"9993-9-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"some-name-2\" \"some-desc-2\" \"2563-12-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"some-name-3\" \"some-desc-3\" \"2013-10-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"some-name-4\" \"some-desc-4\" \"23-1-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"some-name-5\" \"some-desc-5\" \"0-0-5 19:05\" \"cat1\""));
-
-		listener->Do(listener->ParseInput("add \"any-name-0\" \"any-desc-5\" \"400-0-5 19:05\" \"cat0\""));
-		listener->Do(listener->ParseInput("add \"any-name-1\" \"any-desc-2\" \"0-0-5 19:05\" \"cat1\""));
-		listener->Do(listener->ParseInput("add \"any-name-2\" \"any-desc-2\" \"0-0-5 19:05\" \"cat2\""));
 
 		std::string input = "";
 		while (input != exit)
